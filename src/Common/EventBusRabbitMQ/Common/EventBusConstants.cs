30d242b891a63d914b7b169572fe31032218aaa2
Luca D'Angelo <ldazure@hotmail.it>
2021-09-19 22:32:26 +0100

    Add project files.

---
+++ src/Common/EventBusRabbitMQ/Common/EventBusConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBusRabbitMQ.Common
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketCheckoutQueue";
+        public const string SecondConsumerQueue = "secondConsumerQueue";
+    }
+}
